7e861b452e1796304590c6e6fde0be366a39c90e
daniellonsdale <danielllonsdale@gmail.com>
2025-01-31 00:07:19 +0900

    Create init class and constructor

---
+++ src/todo.js
@@ -0,0 +1,8 @@
+class todo{
+    constructor(title, description, dueDate, priority){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
