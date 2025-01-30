67a188f489238e94dc150bf46301b9a134f11c9c
daniellonsdale <danielllonsdale@gmail.com>
2025-01-31 00:30:16 +0900

    Add index property to match with todo array and html data-index

--- src/todo.js
+++ src/todo.js
@@ -1,8 +1,9 @@
 class todo{
-    constructor(title, description, dueDate, priority){
+    constructor(title, description, dueDate, priority, index){
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
+        this.index = index;
     }
 }
