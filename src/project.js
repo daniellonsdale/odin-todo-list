949030ff7436fdd786613aaae13e275f6d4aebeb
daniellonsdale <danielllonsdale@gmail.com>
2025-01-31 00:47:34 +0900

    Rename todos to project

---
+++ src/project.js
@@ -0,0 +1,17 @@
+import { Todo } from "./todo";
+
+export class Project{
+    constructor(){
+        todos = [];
+    }
+
+    createTodo(title, description, dueDate, priority){
+        todos[todos.length] = new Todo(title, description, dueDate, priority, todos.length);
+        //Call createTodoDOM() to add to DOM with data-index
+    }
+
+    removeTodo(index){
+        delete todos[index];
+        //Call removeTodoDOM to remove from DOM based on data-index
+    }
+}
