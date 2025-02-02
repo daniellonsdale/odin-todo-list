649b7796bc98cbed648e698f96a39bcb3c74770b
daniellonsdale <danielllonsdale@gmail.com>
2025-02-02 18:27:39 +0900

    Make create project function

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ import "./styles.css";
 import { Project } from "./project";
 
 let projects = [];
+
+function createProject(name){
+    projects[projects.length] = new Project(name, projects.length);
+    //Call createProjectDOM to add to DOM with data-index
+}
