49439ef2ac56d46f4fb796d2f62b020083ec852e
daniellonsdale <danielllonsdale@gmail.com>
2025-02-02 18:29:23 +0900

    Make removeProject()

--- src/index.js
+++ src/index.js
@@ -7,3 +7,13 @@ function createProject(name){
     projects[projects.length] = new Project(name, projects.length);
     //Call createProjectDOM to add to DOM with data-index
 }
+
+function removeProject(ind){
+    projects.splice(ind, 1);
+    projects.forEach(element => {
+        if (element.index > ind){
+            element.index--;
+        }
+    });
+    //Call removeProjectDOM to remove from DOM based on data-index
+}
