9a270b5ef11a20415202a461606f50cfdbd77468
daniellonsdale <danielllonsdale@gmail.com>
2025-02-02 18:56:59 +0900

    Add this statments before todos to call the instance of the array

--- src/project.js
+++ src/project.js
@@ -1,20 +1,20 @@
 import { Todo } from "./todo";
 
 export class Project{
+    todos = [];
     constructor(name, index){
-        const todos = [];
         this.name = name;
         this.index = index;
     }
 
     createTodo(title, description, dueDate, priority){
-        todos[todos.length] = new Todo(title, description, dueDate, priority, todos.length);
+        this.todos[this.todos.length] = new Todo(title, description, dueDate, priority, this.todos.length);
         //Call createTodoDOM() to add to DOM with data-index
     }
 
     removeTodo(ind){
-        todos.splice(ind, 1);
-        todos.forEach(element => {
+        this.todos.splice(ind, 1);
+        this.todos.forEach(element => {
             if (element.index > ind){
                 element.index--;
             }
