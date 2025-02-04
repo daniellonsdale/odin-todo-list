e03da555a79ef5e87750bd988103a7ea16368278
daniellonsdale <danielllonsdale@gmail.com>
2025-02-05 00:14:11 +0900

    Create updateDisplayedTodos()

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const newProjectSubmitBtn = document.querySelector('.new-project-submit-button')
 const newTaskSubmitBtn = document.querySelector('.new-task-submit-button');
 const defaultProject = document.querySelector('.default-project');
 const projectContainer = document.querySelector('.project-container');
+const tasksContainer = document.querySelector('.tasks-container');
 
 let projects = [];
 let curSelectedProject = 0;
@@ -55,6 +56,57 @@ function removeProjectDOM(ind){
             element.dataset.index = tempIndex;
         }
     });
+}
+
+function updateDisplayedTodos(){
+    tasksContainer.replaceChildren();
+
+    const tempTaskCard = document.createElement('div');
+    tempTaskCard.classList.add('task-card');
+    //Add data-index of index
+
+    const tempTaskCardLeftContainer = document.createElement('div');
+    tempTaskCardLeftContainer.classList.add('task-card-left-container');
+    const tempTaskCardTaskName = document.createElement('div');
+    tempTaskCardTaskName.classList.add('task-name');
+    //Change textContent to name
+    tempTaskCardLeftContainer.appendChild(tempTaskCardTaskName);
+    const tempTaskCardTaskDate = document.createElement('div');
+    tempTaskCardTaskDate.classList.add('task-date');
+    //Change textContent to date
+    tempTaskCardLeftContainer.appendChild(tempTaskCardTaskDate);
+    tempTaskCard.appendChild(tempTaskCardLeftContainer);
+
+    const tempTaskCardRightContainer = document.createElement('div');
+    tempTaskCardRightContainer.classList.add('task-card-right-container');
+
+    const tempTaskCardCompleteContainer = document.createElement('div');
+    tempTaskCardCompleteContainer.classList.add('complete-task');
+    const completeTaskSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    completeTaskSVG.setAttribute('height', '24px');
+    completeTaskSVG.setAttribute('viewbox', '0 -960 960 960');
+    completeTaskSVG.setAttribute('width', '24px');
+    completeTaskSVG.setAttribute('fill', 'currentcolor');
+    const completeTaskPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    completeTaskPath.setAttribute('d', 'M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z');
+    completeTaskSVG.appendChild(completeTaskPath);
+    tempTaskCardCompleteContainer.appendChild(completeTaskSVG);
+    tempTaskCardRightContainer.appendChild(tempTaskCardCompleteContainer);
+
+    const tempTaskCardDeleteContainer = document.createElement('div');
+    tempTaskCardDeleteContainer.classList.add('delete-task');
+    const deleteTaskSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    deleteTaskSVG.setAttribute('height', '24px');
+    deleteTaskSVG.setAttribute('viewbox', '0 -960 960 960');
+    deleteTaskSVG.setAttribute('width', '24px');
+    deleteTaskSVG.setAttribute('fill', 'currentcolor');
+    const deleteTaskPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    deleteTaskPath.setAttribute('d', 'M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z');
+    deleteTaskSVG.appendChild(deleteTaskPath);
+    tempTaskCardDeleteContainer.appendChild(deleteTaskSVG);
+    tempTaskCardRightContainer.appendChild(tempTaskCardDeleteContainer);
+    tempTaskCard.appendChild(tempTaskCardRightContainer);
+
 }
 
 addNewProjectBtn.addEventListener('click', () => {
