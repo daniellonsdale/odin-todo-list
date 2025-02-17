647e6123a10679f4f4ce7830f740b4c8d37c2498
daniellonsdale <danielllonsdale@gmail.com>
2025-02-17 18:29:59 +0900

    Add static method for rehydrating todo objects

--- src/todo.js
+++ src/todo.js
@@ -7,4 +7,8 @@ export class Todo{
         this.index = index;
         this.completed = completed;
     }
+
+    static fromJSON(json) {
+        return new Todo(json.title, json.description, json.dueDate, json.priority, json.index, json.completed);
+    }
 }
