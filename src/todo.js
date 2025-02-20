40e475aa6b36385807c2369a79b67803c047546a
daniellonsdale <danielllonsdale@gmail.com>
2025-02-21 00:52:58 +0900

    Remove unneeded static method

--- src/todo.js
+++ src/todo.js
@@ -7,8 +7,4 @@ export class Todo{
         this.index = index;
         this.completed = completed;
     }
-
-    static fromJSON(json) {
-        return new Todo(json.title, json.description, json.dueDate, json.priority, json.index, json.completed);
-    }
 }
